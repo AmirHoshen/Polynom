adb3afe0f986f68883be98c7f096cde7c2e40d9e
Peleg Z <peleg122@gmail.com>
2019-11-26 18:56:09 +0200

    Added Functions_GUITest functions

--- Polynom/Ex1/test/complexFunctionTest.java
+++ Polynom/Ex1/test/complexFunctionTest.java
@@ -1,5 +1,9 @@
 package Ex1.test;
 
+import Ex1.ComplexFunction;
+import Ex1.Monom;
+import Ex1.Polynom;
+import Ex1.function;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -62,5 +66,41 @@ class complexFunctionTest {
 
     @Test
     void copy() {
+    }
+    public static Functions_GUI FunctionsFactory() throws Exception {
+        Functions_GUI ans = new Functions_GUI();
+        String s1 = "3.1 +2.4x^2 -x^4";
+        String s2 = "5 +2x -3.3x +0.1x^5";
+        String[] s3 = {"x +3","x -2", "x -4"};
+        Polynom p1 = new Polynom(s1);
+        Polynom p2 = new Polynom(s2);
+        Polynom p3 = new Polynom(s3[0]);
+        ComplexFunction cf3 = new ComplexFunction(p3);
+        for(int i=1;i<s3.length;i++) {
+            cf3.mul(new Polynom(s3[i]));
+        }
+
+        ComplexFunction cf = new ComplexFunction("plus", p1,p2);
+        ComplexFunction cf4 = new ComplexFunction("div", new Polynom("x +1"),cf3);
+        cf4.plus(new Monom("2"));
+        ans.add(cf.copy());
+        ans.add(cf4.copy());
+        cf.div(p1);
+        ans.add(cf.copy());
+        String s = cf.toString();
+        function cf5 = cf4.initFromString(s1);
+        function cf6 = cf4.initFromString(s2);
+        ans.add(cf5.copy());
+        ans.add(cf6.copy());
+        ComplexFunction max = new ComplexFunction(ans.get(0).copy());
+        ComplexFunction min = new ComplexFunction(ans.get(0).copy());
+        for(int i=1;i<ans.size();i++) {
+            max.max(ans.get(i));
+            min.min(ans.get(i));
+        }
+        ans.add(max);
+        ans.add(min);
+
+        return ans;
     }
 }
