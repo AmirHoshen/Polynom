d4f13ef15a0668f9600dd85eb6d973d5cee5ea64
peleg <peleg122@gmail.com>
2019-11-30 16:26:57 +0200

    few Test Before Building The ComplexFunction

--- Polynom/Ex1/test/complexFunctionTest.java
+++ Polynom/Ex1/test/complexFunctionTest.java
@@ -1,106 +1,133 @@
-package Ex1.test;
 
 import Ex1.ComplexFunction;
-import Ex1.Monom;
 import Ex1.Polynom;
-import Ex1.function;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 class complexFunctionTest {
 
     @BeforeAll
-    static void setUpBeforeClass() throws Exception {
+    static void setUpBeforeClass()  {
         System.out.println("Start complexFunction Test \n");
     }
 
     @AfterAll
-    static void tearDownAfterClass() throws Exception {
+    static void tearDownAfterClass()  {
         System.out.println("End complexFunction Test");
     }
 
     @Test
-    void plus() {
+    void plus() throws Exception {
+        Polynom p1 = new Polynom("x+2");
+        Polynom p2 = new Polynom("5x+5");
+        ComplexFunction cf = new ComplexFunction("Plus",p1,p2);
+        p1.add(p2);
+        if(!p1.toString().equals(cf.toString())){
+            fail("Not Equal Strings of Plus");
+        }else{
+            System.out.println("Equal String of Plus");
+        }
     }
 
     @Test
-    void mul() {
+    void mul() throws Exception {
+        Polynom p1 = new Polynom("2x^2+2");
+        Polynom p2 = new Polynom("x+5");
+        ComplexFunction cf = new ComplexFunction("Times",p1,p2);
+        p1.multiply(p2);
+        if(!p1.toString().equals(cf.toString())){
+            fail("Not Equal Strings of Multiply");
+        }else{
+            System.out.println("Equal String of Multiply");
+        }
     }
 
     @Test
-    void div() {
+    void div() throws Exception {
     }
 
     @Test
-    void max() {
+    void max() throws Exception {
     }
 
     @Test
-    void min() {
+    void min() throws Exception {
     }
 
     @Test
-    void comp() {
+    void comp() throws Exception {
     }
 
     @Test
-    void left() {
+    void left() throws Exception {
+        Polynom p1 = new Polynom();
+        Polynom p2 = new Polynom();
+        Polynom p3 = new Polynom();
+        ComplexFunction cf = new ComplexFunction("plus",new ComplexFunction("plus",p1,p2),p3);
+        ComplexFunction cfLeft = new ComplexFunction(cf.left());
+
     }
 
     @Test
-    void right() {
+    void right() throws Exception {
     }
 
     @Test
-    void getOp() {
+    void getOp() throws Exception {
     }
 
     @Test
-    void f() {
+    void f() throws Exception {
     }
 
     @Test
-    void initFromString() {
+    void initFromString() throws Exception {
     }
 
     @Test
-    void copy() {
+    void copy() throws Exception {
     }
-    public static Functions_GUI FunctionsFactory() throws Exception {
-        Functions_GUI ans = new Functions_GUI();
-        String s1 = "3.1 +2.4x^2 -x^4";
-        String s2 = "5 +2x -3.3x +0.1x^5";
-        String[] s3 = {"x +3","x -2", "x -4"};
+@Test
+     void FunctionsFactory() throws Exception {
+        //Functions_GUI ans = new Functions_GUI();
+        String s1 = "-x^4+2x^3+x+2";
+        String s2 = "x+2";
+        String[] s3 = {"x+3","x-2", "x-4"};
         Polynom p1 = new Polynom(s1);
         Polynom p2 = new Polynom(s2);
         Polynom p3 = new Polynom(s3[0]);
-        ComplexFunction cf3 = new ComplexFunction(p3);
+        //ComplexFunction cf3 = new ComplexFunction(p3);
         for(int i=1;i<s3.length;i++) {
-            cf3.mul(new Polynom(s3[i]));
+         //  cf3.mul(new Polynom(s3[i]));
         }
 
         ComplexFunction cf = new ComplexFunction("plus", p1,p2);
-        ComplexFunction cf4 = new ComplexFunction("div", new Polynom("x +1"),cf3);
-        cf4.plus(new Monom("2"));
-        ans.add(cf.copy());
-        ans.add(cf4.copy());
+        //ComplexFunction cf4 = new ComplexFunction("div", new Polynom("x +1"),cf3);
+       // cf4.plus(new Monom("2"));
+       // cf4.plus(new Polynom("x+2"));
+        //ans.add(cf.copy());
+        //ans.add(cf4.copy());
         cf.div(p1);
-        ans.add(cf.copy());
+        cf.plus(new Polynom("3x^5+2x+3"));
+        //ans.add(cf.copy());
         String s = cf.toString();
-        function cf5 = cf4.initFromString(s1);
-        function cf6 = cf4.initFromString(s2);
-        ans.add(cf5.copy());
-        ans.add(cf6.copy());
-        ComplexFunction max = new ComplexFunction(ans.get(0).copy());
-        ComplexFunction min = new ComplexFunction(ans.get(0).copy());
-        for(int i=1;i<ans.size();i++) {
-            max.max(ans.get(i));
-            min.min(ans.get(i));
-        }
-        ans.add(max);
-        ans.add(min);
+        System.out.println(s);
+        //function cf5 = cf4.initFromString(s1);
+        //function cf6 = cf4.initFromString(s2);
+        //ans.add(cf5.copy());
+        //ans.add(cf6.copy());
+        //ComplexFunction max = new ComplexFunction(ans.get(0).copy());
+        //ComplexFunction min = new ComplexFunction(ans.get(0).copy());
+        //for(int i=1;i<ans.size();i++) {
+        //    max.max(ans.get(i));
+        //    min.min(ans.get(i));
+        //}
+        //ans.add(max);
+        //ans.add(min);
 
-        return ans;
+        //return ans;
     }
 }
