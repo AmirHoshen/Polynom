2a5c7aa076ea59ca7cadab5a619345a659137a2d
amir hoshen <mi.rosh160@gmail.com>
2019-12-04 14:47:48 +0200

    Functions_GUI initialized

---
+++ Polynom/Ex1/Functions_GUI.java
@@ -0,0 +1,135 @@
+package Ex1;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Functions_GUI implements functions{
+    public ArrayList<function> list = new ArrayList<function>();
+
+    @Override
+    public boolean add(function e) {
+        if(contains(e))return false;
+        return this.list.add(e);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends function> c) {
+        return this.list.addAll(c);
+    }
+
+    @Override
+    public void clear() {
+        this.list.clear();
+
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return this.list.contains(o);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return this.list.containsAll(c);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.list.isEmpty();
+    }
+
+    @Override
+    public Iterator<function> iterator() {
+        return this.list.iterator();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return this.list.remove(o);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return this.list.removeAll(c);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return this.list.retainAll(c);
+    }
+
+    @Override
+    public int size() {
+        return this.list.size();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return this.list.toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return this.list.toArray(a);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < this.size(); i++) {
+            sb.append(this.list.get(i).toString());
+            if(i < size() - 1) sb.append(", ");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public void initFromFile(String file) throws IOException {
+        // TODO Auto-generated method stub
+        // variable declaration
+        int ch;
+
+        // check if File exists or not
+        FileReader fr=null;
+        try
+        {
+            fr = new FileReader(file);
+        }
+        catch (FileNotFoundException fe)
+        {
+            System.out.println("File not found");
+        }
+
+        // read from FileReader till the end of file
+        while ((ch=fr.read())!=-1)
+            System.out.print((char)ch);
+
+        // close the file
+        fr.close();
+    }
+
+
+
+    @Override
+    public void saveToFile(String file) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void drawFunctions(int width, int height, Range rx, Range ry, int resolution) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void drawFunctions(String json_file) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+}
