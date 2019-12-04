0a935f84479fa5649573f792431734e78efa103e
amir hoshen <mi.rosh160@gmail.com>
2019-12-04 14:47:15 +0200

    functions_GUITest empty

---
+++ Polynom/Ex1/test/Functions_GUITest.java
@@ -0,0 +1,24 @@
+package Ex1.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Functions_GUITest {
+
+    @Test
+    void initFromFile() {
+    }
+
+    @Test
+    void saveToFile() {
+    }
+
+    @Test
+    void drawFunctions() {
+    }
+
+    @Test
+    void testDrawFunctions() {
+    }
+}
