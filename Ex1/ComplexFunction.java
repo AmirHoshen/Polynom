31516357bf54883b4ae07ca9ade4e0ed739046c2
Peleg Z <peleg122@gmail.com>
2019-12-11 17:39:54 +0200

    Rearranged the Folder

---
+++ Ex1/ComplexFunction.java
@@ -0,0 +1,358 @@
+package Ex1;
+
+import java.util.Stack;
+
+import static Ex1.Operation.*;
+public class ComplexFunction implements complex_function{
+    private Operation operation;
+    private function left;
+    private function right;
+
+    /***
+     * Empty ComplexFunction Constructor
+     * which is default to be null in its left and right functions and Error Operation
+     */
+    public ComplexFunction(){
+        operation = Error;
+        left = null;
+        right = null;
+    }
+
+    /***
+     * most useful constructor builds the ComplexFunction to the fullest
+     * at the beginning the String get checked for it's value and compared with different Operation names
+     * in addition it would check for the words mul or div and not only 'Times' or 'Divid' from the Operation enum
+     * and after that the functions left and right gets to be put in there place according to the order inserted
+     *  using break to stop extra search!
+     * @param op - String with the operation word to be preformed on the functions left and right
+     * @param left - ComplexFunction/Monom/Polynom function to be inserted in the left function of ComplexFunction
+     * @param right - ComplexFunction/Monom/Polynom function to be inserted in the right function of ComplexFunction
+     */
+    public ComplexFunction(String op, function left, function right)  {
+        this.operation=Error;
+        for(Operation operation : Operation.values()){
+            if(op.equalsIgnoreCase("mul")) {
+                this.operation = Times;
+                break;
+            }
+            if(op.equalsIgnoreCase("div")) {
+                this.operation = Divid;
+                break;
+            }
+            if(operation.name().equalsIgnoreCase(op)){
+                this.operation = operation;
+                break;
+            }
+        }
+        this.left = left;
+        this.right = right;
+    }
+
+    /***
+     * Does The Same as the ComplexFunction(String ,function, function) but takes the operation directly
+     * @param op - Operation to be preformed on the functions left and right
+     * @param left - ComplexFunction/Monom/Polynom function to be inserted in the left function of ComplexFunction
+     * @param right - ComplexFunction/Monom/Polynom function to be inserted in the right function of ComplexFunction
+     */
+    public ComplexFunction(Operation op, function left, function right)  {
+        ComplexFunction _temp = new ComplexFunction(op.name(),left,right);
+        this.operation = _temp.getOp();
+        this.left = _temp.left();
+        this.right = _temp.right();
+    }
+
+    public ComplexFunction(function function) {
+        if(function instanceof ComplexFunction){
+            this.operation = ((ComplexFunction) function).getOp();
+            this.left = new ComplexFunction( ((ComplexFunction) function).left());
+            this.right = new ComplexFunction(((ComplexFunction) function).right());
+        }else if(function instanceof Polynom){
+            this.operation = None;
+            this.left = function;
+            this.right = null;
+        }else if(function instanceof Monom){
+            this.operation = None;
+            this.left = function;
+            this.right = null;
+        }
+    }
+
+    /***
+     * Using two Stacks to count how many brackets are in the string and if they're in the right order
+     * @param s - String to be check for brackets delivered from initToString()
+     * @return - returns true if the brackets are in order and in the right amount false if not
+     */
+    public static boolean isBalanced(String s){
+        Stack<Character> leftChar = new Stack<>();
+        Stack<Character> rightChar = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='(') leftChar.push(s.charAt(i));
+            if(s.charAt(i)==')') rightChar.push(s.charAt(i));
+            if(leftChar.size() < rightChar.size())
+                return false;
+        }
+        return leftChar.size() == rightChar.size();
+    }
+
+    /** Add to this complex_function the f1 complex_function
+     * @param f1 the complex_function which will be added to this complex_function.
+     */
+    @Override
+    public void plus(function f1)  {
+        if(right!=null){
+            ComplexFunction prevComplex = new ComplexFunction(getOp(),left(),right());
+            this.left =  prevComplex;
+        }
+        this.right = new ComplexFunction(f1);
+        this.operation = Plus;
+    }
+
+    /** Multiply this complex_function with the f1 complex_function
+     * @param f1 the complex_function which will be multiply be this complex_function.
+     */
+    @Override
+    public void mul(function f1){
+        if(right!=null){
+            ComplexFunction prevComplex = new ComplexFunction(getOp(),left(),right());
+            this.left =  prevComplex;
+        }
+        this.right = new ComplexFunction(f1);
+        this.operation = Times;
+
+    }
+    /** Divides this complex_function with the f1 complex_function
+     * @param f1 the complex_function which will be divid this complex_function.
+     */
+    @Override
+    public void div(function f1){
+        if(right!=null){
+            ComplexFunction prevComplex = new ComplexFunction(getOp(),left(),right());
+            this.left =  prevComplex;
+        }
+        this.right = new ComplexFunction(f1);
+        this.operation = Divid;
+
+    }
+    /** Computes the maximum over this complex_function and the f1 complex_function
+     * @param f1 the complex_function which will be compared with this complex_function - to compute the maximum.
+     */
+    @Override
+    public void max(function f1){
+        if(right!=null){
+            ComplexFunction prevComplex = new ComplexFunction(getOp(),left(),right());
+            this.left =  prevComplex;
+        }
+        this.right = new ComplexFunction(f1);
+        this.operation = Max;
+    }
+    /** Computes the minimum over this complex_function and the f1 complex_function
+     * @param f1 the complex_function which will be compared with this complex_function - to compute the minimum.
+     */
+    @Override
+    public void min(function f1){
+        if(right!=null){
+            ComplexFunction prevComplex = new ComplexFunction(getOp(),left(),right());
+            this.left =  prevComplex;
+        }
+        this.right = new ComplexFunction(f1);
+        this.operation = Min;
+    }
+    /**
+     * This method wrap the f1 complex_function with this function: this.f(f1(x))
+     * @param f1 complex function
+     */
+    @Override
+    public void comp(function f1){
+        if(right!=null){
+            ComplexFunction prevComplex = new ComplexFunction(getOp(),left(),right());
+            this.left =  prevComplex;
+        }
+        this.right = new ComplexFunction(f1);
+        this.operation = Comp;
+    }
+    /** returns the left side of the complex function - this side should always exists (should NOT be null).
+     * @return a function representing the left side of this complex funcation
+     */
+    @Override
+    public function left(){
+        return this.left;
+    }
+    /** returns the right side of the complex function - this side might not exists (aka equals null).
+     * @return a function representing the left side of this complex funcation
+     */
+    @Override
+    public function right(){
+        return this.right;
+    }
+
+    /**
+     * The complex_function operation: plus, mul, div, max, min, comp ,none
+     * @return op - returns Operation of the ComplexFunction
+     */
+    @Override
+    public Operation getOp(){
+        return this.operation;
+    }
+
+    /***
+     *
+     * @param x - the number to be in the f(x) to compute the final result
+     * @return - by the last operation it would decide which action to preform and recursively get the number from the
+     * left and right functions according to the ComplexFunction order of build
+     * @throws ArithmeticException if there is an attempt to divide a number with zero
+     */
+    @Override
+    public double f(double x) {
+        switch (operation) {
+            case Plus:
+                return left.f(x) + right.f(x);
+            case Times:
+                return left.f(x) * right.f(x);
+            case Divid:
+                if(right.f(x) != 0 )
+                    return left.f(x) / right.f(x);
+                else
+                    throw new ArithmeticException("Can't Divide with zero");
+            case Min:
+                return Math.min(left.f(x),right.f(x));
+            case Max:
+                return Math.max(left.f(x),right.f(x));
+            case Comp:
+                return left.f(right.f(x));
+            case None:
+                return left.f(x);
+            default:
+                throw new RuntimeException("Operation is undefined!");
+        }
+    }
+
+    /***
+     * returns ComplexFunction with only Polynom if we get a string with only Polynom string and no brackets
+     * and if it contains any brackets it would get the operation and look for the first opening brackets (excluded)
+     * to indicate where to start the left function and for the appropriate comma (excluded) to end the left side
+     * with the help of the comma we take the string all the way to the last bracket
+     * not included to define the right function.
+     * @param s - String used to build a ComplexFunction
+     * @return - returns ComplexFunction
+     */
+    @Override
+    public function initFromString(String s) {
+        s=s.replaceAll(" ","");
+        if(s.contains("(") && s.contains(")") && isBalanced(s)){
+            int indexOfFirstBracket = s.indexOf('(');
+            int indexOfComma = commaFinder(s);
+            int indexOfLastBracket = s.lastIndexOf(')');
+            return new ComplexFunction(s.substring(0,indexOfFirstBracket),
+                    initFromString(s.substring(indexOfFirstBracket+1,indexOfComma)),
+                    initFromString(s.substring(indexOfComma+1,indexOfLastBracket)));
+        }else{
+            try {
+                return new ComplexFunction(new Polynom(s));
+            }catch (Exception e){e.getMessage();}
+        }
+        return null;// never would get here anyway but makes no errors because of try/catch
+    }
+
+    /***
+     * commaFinder is looking for the comma that belongs to the first bracket
+     * we count the first bracket and each bracket after the first makes the comma after it invisible and only after
+     * it would get the closing bracket it would go back to keep looking for the comma of the first bracket
+     * using break to stop extra search and less operations
+     * @param s - String to be searched for the comma of the first and last brackets
+     * @return - returns index of the right comma in the string
+     */
+    public int commaFinder(String s){
+        int opening = 0;
+        int comma = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i)=='(')
+                opening++;
+            if(s.charAt(i)==')')
+                opening--;
+            if(opening==1 && s.charAt(i)==','){
+                comma = i;
+                break;
+            }
+        }
+        return comma;
+    }
+    /***
+     * creates deep copy of the ComplexFunction
+     * @return - returns ComplexFunction with it's fields copied
+     */
+    @Override
+    public function copy() {
+        ComplexFunction _temp = new ComplexFunction(operation,left,right);
+        return _temp;
+    }
+
+    /***
+     * Creates a String with each operation and the inner left and right String of the functions according to their
+     * toString() function if it a Polynom or Monom Would be using their own toString to create a string of the object.
+     * @return String with the Operation open brackets might be another operation or a Polynom or a Monom String
+     * followed by a comma and another Operation or Polynom or Monom String and a closing brackets
+     */
+    @Override
+    public String toString(){
+        String  ans = "";
+        if(Operation(operation)!="none"){
+            ans = Operation(operation)+"(" + left.toString();
+            if(right!= null)
+                ans += "," + right.toString() + ")";
+        }else{
+            ans = left.toString();
+        }
+        return ans;
+    }
+
+    /***
+     * Operation returns the string according to its relevant Case
+     * Example: if operation = Plus it would return the String "plus"
+     * @param operation - takes Operation and converts it into a String
+     * @return - String of the equivalent operation
+     */
+    public String Operation(Operation operation){
+        switch (operation){
+            case None:
+                return "none";
+            case Plus:
+                return "plus";
+            case Times:
+                return "mul";
+            case Comp:
+                return "comp";
+            case Max:
+                return "max";
+            case Divid:
+                return "div";
+            case Min:
+                return "min";
+            default:
+                return "Error";
+        }
+    }
+
+    /*
+     * plus(9x^3,x^3)==Times(10x,x^2)
+     */
+
+    /**
+     * check if this class complex functions is identical to
+     * Object obj (that should be instance of complex function other wise returns false)
+      * @param obj the complex function to compare with.
+     * @return true if they are identical
+     * otherwise false.
+     */
+    public boolean equals(Object obj) {
+        if(!(obj instanceof ComplexFunction))
+            return false;
+
+        ComplexFunction other = (ComplexFunction) obj;
+
+        for (int i = 1; i <=10; i++) {
+            if(this.f(i)!=other.f(i))
+                return false;
+        }
+        return true;
+    }
+}
---
+++ Ex1Testing/complexFunctionTest.java
@@ -0,0 +1,188 @@
+package Ex1Testing;
+
+import Ex1.ComplexFunction;
+import Ex1.Monom;
+import Ex1.Operation;
+import Ex1.Polynom;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class complexFunctionTest {
+
+    @BeforeAll
+    static void setUpBeforeClass()  {
+        System.out.println("Start complexFunction Test \n");
+    }
+
+    @AfterAll
+    static void tearDownAfterClass()  {
+        System.out.println("End complexFunction Test");
+    }
+
+    @Test
+    void plus() throws Exception {
+        Polynom p1 = new Polynom("x+2");
+        Polynom p2 = new Polynom("5x+5");
+        ComplexFunction cf = new ComplexFunction("Plus",p1,p2);
+        p1.add(p2);
+        if(!p1.toString().equals(cf.toString())){
+            fail("Not Equal Strings of Plus");
+        }else{
+            System.out.println("Equal String of Plus");
+        }
+    }
+
+    @Test
+    void mul() throws Exception {
+        Polynom p1 = new Polynom("2x^2+2");
+        Polynom p2 = new Polynom("x+5");
+        ComplexFunction cf = new ComplexFunction("Times",p1,p2);
+        p1.multiply(p2);
+        if(!p1.toString().equals(cf.toString())){
+            fail("Not Equal Strings of Multiply");
+        }else{
+            System.out.println("Equal String of Multiply");
+        }
+    }
+
+    @Test
+    void div() throws Exception {
+    }
+
+    @Test
+    void max() throws Exception {
+    }
+
+    @Test
+    void min() throws Exception {
+    }
+
+    @Test
+    void comp() throws Exception {
+    }
+
+    @Test
+    void left() throws Exception {
+        Polynom p1 = new Polynom("x+1");
+        Polynom p2 = new Polynom("2x^2+3x");
+        Polynom p3 = new Polynom("4x^4+2");
+        ComplexFunction cf = new ComplexFunction("plus",new ComplexFunction("plus",p1,p2),p3);
+        ComplexFunction cfLeft = new ComplexFunction(cf.left());
+        if(cf.left() == null || cfLeft == null || !cf.left().toString().equals(cfLeft.toString())){
+            fail("Left Function isn't working!");
+        }else{
+            System.out.println("Left Function Working Well");
+        }
+    }
+
+    @Test
+    void right() throws Exception {
+        Polynom p1 = new Polynom("x+5");
+        Polynom p2 = new Polynom("8x^2+3");
+        Polynom p3 = new Polynom("2.5x^3+x+1");
+        ComplexFunction cf = new ComplexFunction("plus",new ComplexFunction("plus",p1,p2),p3);
+        ComplexFunction cfRight = new ComplexFunction(cf.right());
+        if(cf.right() == null || cfRight == null ||!cf.right().toString().equals(cfRight.toString())){
+            fail("Right Function isn't working!");
+        }else{
+            System.out.println("Right Function Working Well");
+        }
+    }
+
+    @Test
+    void getOp() throws Exception {
+        String op = "times";
+        Operation expected = Operation.Times;
+        ComplexFunction cf = new ComplexFunction(op,new Polynom("x+2"),new Polynom("2x^2+4"));
+        ComplexFunction cf2 = new ComplexFunction(cf);
+        if(cf.getOp() != expected && cf2.getOp() != expected){
+            fail("Expected Operation: " + expected + " Actual: " + cf.getOp() +","+cf2.getOp());
+        }else {
+            System.out.println("Expected Operation: " + expected.name() + " Actual: " + cf.getOp() +","+cf2.getOp());
+        }
+    }
+
+    @Test
+    void f() throws Exception {
+    }
+
+    @Test
+    void initFromStringTest() throws Exception {
+        ComplexFunction _init = new ComplexFunction();
+        _init.initFromString("plus(-1.0x^4 +2.4x^2 +3.1,+0.1x^5 -1.2999999999999998x +5.0)");
+        System.out.println(_init.toString());
+    }
+
+    @Test
+    void copy()  {
+    }
+@Test
+    void FunctionsFactory() throws Exception {
+    ComplexFunction cf = new ComplexFunction("plus",new ComplexFunction("plus", new Polynom("6x^2+3x+2"),new Polynom("3x^2+3X+2")),new ComplexFunction("plus",new ComplexFunction("times",new Polynom("x+1"),new Monom("2x")),new Polynom("x")));
+    ComplexFunction cf1 = new ComplexFunction("times",new Polynom("8x+8"),new Polynom("9x+9"));
+    ComplexFunction cf2 = new ComplexFunction("max",new ComplexFunction("min",new Polynom("3x+2"),new Polynom("2x")),new Monom("7x^3"));
+    System.out.println("Testing toString(): ");
+    System.out.println("cf is: "+cf.toString()+"\ncf1 is: "+cf1.toString()+"\ncf2 is: "+cf2.toString());
+    System.out.println("Testing f(x): ");
+    System.out.println("cf --> f(3)= " + cf.f(3));
+    System.out.println("cf1 --> f(3)= " + cf1.f(3));
+    System.out.println("Testing equals: ");
+    ComplexFunction cf3 = new ComplexFunction(cf2);
+    System.out.println("functions is identical?:\n" +
+            "Expected: true\n" +
+            "Actual: "+cf2.equals(cf3));
+
+
+    //ComplexFunction cf = new ComplexFunction("plus", new Polynom("2x+1"),new Polynom("2x+1"));
+        //Functions_GUI ans = new Functions_GUI();
+//        String s1 = "-x^4+2x^3+x+2";
+//        String s2 = "x+2";
+//        String[] s3 = {"x+3","x-2", "x-4"};
+//        Polynom p1 = new Polynom(s1);
+//        Polynom p2 = new Polynom(s2);
+//        Polynom p3 = new Polynom(s3[0]);
+        //ComplexFunction cf3 = new ComplexFunction(p3);
+//        for(int i=1;i<s3.length;i++) {
+         //  cf3.mul(new Polynom(s3[i]));
+//        }
+
+//        ComplexFunction cf = new ComplexFunction("plus",
+//                new ComplexFunction("times",
+//                        new ComplexFunction("times",new Polynom("2x+2"),new Polynom("3x+3")),
+//                        new ComplexFunction("times",new Polynom("8x+8"),new Polynom("9x+9"))
+//                ),
+//                new ComplexFunction("times",
+//                        new ComplexFunction("times",new Polynom("4x+4"),new Polynom("5x+5")),
+//                        new ComplexFunction("times",new Polynom("6x+6"),new Polynom("7x+7"))
+//                )
+//        );
+
+        //ComplexFunction cf4 = new ComplexFunction("div", new Polynom("x +1"),cf3);
+       // cf4.plus(new Monom("2"));
+       // cf4.plus(new Polynom("x+2"));
+        //ans.add(cf.copy());
+        //ans.add(cf4.copy());
+        //cf.div(p1);
+        //cf.plus(new Polynom("3x^5+2x+3"));
+        //ans.add(cf.copy());
+       // String s = cf.toString();
+        //System.out.println(s);
+        //function cf5 = cf4.initFromString(s1);
+        //function cf6 = cf4.initFromString(s2);
+        //ans.add(cf5.copy());
+        //ans.add(cf6.copy());
+        //ComplexFunction max = new ComplexFunction(ans.get(0).copy());
+        //ComplexFunction min = new ComplexFunction(ans.get(0).copy());
+        //for(int i=1;i<ans.size();i++) {
+        //    max.max(ans.get(i));
+        //    min.min(ans.get(i));
+        //}
+        //ans.add(max);
+        //ans.add(min);
+
+        //return ans;
+    }
+}
